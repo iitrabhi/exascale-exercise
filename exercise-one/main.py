1ca3e24e6d0aa052c36f809714160735e1740cda
Abhinav Gupta <iitrabhi@gmail.com>
2022-07-12 18:47:14 +0530

    set data exchange between processors

--- exercise-one/main.py
+++ exercise-one/main.py
@@ -11,8 +11,18 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-N = 10
-M = 5
+# print(size)
+
+N = 10 # Range [0,N)
+M = 3 # Size of set with each processor
 
 np.random.seed(rank)
-print("This is processor ", rank, " and output", np.random.choice(N, M, replace=False))
+data = np.random.choice(N, M, replace=False)
+print("This is processor ", rank, " and output", data)
+# Send data to other destinations
+for i in range(rank+1,size):
+    comm.Send([data, MPI.INT], dest=i)
+# Receive data from other destinations
+for i in range(0,rank):
+    comm.Recv([data, MPI.INT], source=i)
+    print("This is processor ", rank, " and received", data, " from processor ", i)

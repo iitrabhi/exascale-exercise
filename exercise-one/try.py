b678cd866b3e6831cd05c05072a23417d143a257
iitrabhi <iitrabhi@gmail.com>
2022-07-13 14:55:49 +0530

    remove description

--- exercise-one/try.py
+++ exercise-one/try.py
@@ -1,9 +1,3 @@
-# Create an MPI program that generates a set of M random integers
-# (indices) in the range [0,N), on each process (rank). The random
-# number generator is seeded differently on each rank. For each
-# index on a rank, the program must be able to compute which other
-# ranks also have that index. Bear in mind that M and the number of
-# ranks may be large.
 import numpy as np
 from mpi4py import MPI
 

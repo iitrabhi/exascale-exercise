99ec65d0cb8be864b60a46d9ec2556fe3c384821
iitrabhi <iitrabhi@gmail.com>
2022-07-13 15:06:13 +0530

    Update try.py

--- exercise-one/try.py
+++ exercise-one/try.py
@@ -18,16 +18,16 @@ data = np.random.choice(N, M, replace=False)
 print("This is processor ", rank, " and output", data)
 # Send data to other processors
 for i in range(0, size):
-    if not i==rank:
+    if not i == rank:
         comm.Send(data, dest=i, tag=4)
 
 # Receive data from other processors
 for i in range(0, size):
-    if not i==rank:
-        received_data =  np.empty(M, dtype = data.dtype)
+    if not i == rank:
+        received_data = np.empty(M, dtype=data.dtype)
         comm.Recv(received_data, source=i, tag=4)
         common_elements = np.intersect1d(data, received_data)
-        for ele in common_elements: 
+        for ele in common_elements:
             check[data == ele, i] = 1
-    
+
 print("This is processor ", rank, " and check matrix \n", check)
